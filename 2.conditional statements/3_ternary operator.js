642c4b5a561c3841c8815d9c519a05070ddaf54b
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-14 23:36:35 +0530

    doing the conditional statement question and learn about alert and prompt box

---
+++ 2.conditional statements/3_ternary operator.js
@@ -0,0 +1,3 @@
+//ternary operator is just like the short form of if else 
+    let age=25;
+age>=18?"you are adult":"you are not adult"
---
+++ 2.conditional statements/questions/check the number is multiple of 5 or not using prompt.js
@@ -0,0 +1,7 @@
+//alert("hello") //this give the alert
+let a=prompt("Enter the number")//this also give prompt box
+if(a%5==0){
+    console.log(a,"number is multiple of 5")
+}else{
+    console.log(a,"number is not multiple of 5")
+}
