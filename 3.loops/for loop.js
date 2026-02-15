3cb0c98910f4b85d545a43a4762da73bdb6edbdf
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-15 22:24:51 +0530

    learning the for loop

---
+++ 3.loops/for loop.js
@@ -0,0 +1,16 @@
+//there are meinly two types of loops (exit control and entry control)
+//loops are used to exicute piece of code again and again (there is for loop ,while loop and do while loop)
+//for loop 
+// for (let i=0;i<5;i++){
+//     console.log("Varun thakur")
+// }
+// console.log("loop has ending")
+//this is the basic example of the loop 
+//calcululate the sum 1 to 10
+let a=0;
+for (let i=1;i<=10;i++){
+ a=a+i
+
+}
+console.log(a)
+//infinite loop have not last ending condition(a loop never ends)
