f4847b315156f6ba1abf9d5d6c11bdbc3d06e194
Varun Thakur <vtthakurvarun@gmail.com>
2026-02-15 22:31:17 +0530

    learning the while and do while loop

---
+++ 3.loops/2_while loop.js
@@ -0,0 +1,6 @@
+//in the wile loop the initilization condition is before loop and the updation inside the loop 
+let i=1;
+while(i<=5){
+    console.log(i)
+    i++
+}
---
+++ 3.loops/3_do while loop .js
@@ -0,0 +1,6 @@
+// in this loop the loop work atleast one time
+let i=5
+do{
+    console.log(i)
+    i++
+}while(i<=5)
