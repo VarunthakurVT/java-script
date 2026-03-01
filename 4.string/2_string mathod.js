acbc672be235f50f2da5282a3bbe6363bfe44ec6
Varun Thakur <vtthakurvarun@gmail.com>
2026-03-01 21:47:42 +0530

    learning the string methods

--- 4.string/2_string mathod.js
+++ 4.string/2_string mathod.js
@@ -1,5 +1,9 @@
 let name="varun   thakur";
+let age="18"
 console.log(name.toUpperCase());//these method not change the original string it create new string 
 console.log(name.toLowerCase());
 console.log(name.trim());
 console.log(name.slice(0,5))
+console.log(name.concat(age)) //we can do concatination by use +
+console.log(name.replace("varun","xyz")) //replace all to replace all value
+console.log(name.charAt(0))
